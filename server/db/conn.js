f491a234eba6aa9fa9eb3b51e6a2c08705ef41ed
usha-here <usha.20225099@mnnit.ac.in>
2025-10-04 10:32:15 +0530

    changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,10 +2,7 @@ const mongoose=require('mongoose');
 
 const DB=process.env.DATABASE;
 
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-}).then(()=>{
+mongoose.connect(DB).then(()=>{
     console.log('Connected to database');
 }).catch((err)=>{
     console.log("Error connecting to database",err);
