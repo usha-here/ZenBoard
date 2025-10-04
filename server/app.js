6144181595ac54ae713a2b12bb5c11b95224565d
usha-here <usha.20225099@mnnit.ac.in>
2025-10-04 17:13:47 +0530

    passport initialized

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const app=express();
 const cors=require('cors');
 const PORT=3000;
 require('./db/conn');
+const session = require("express-session");
+const passport = require("passport");
+const OAuth2Strategy = require("passport-google-oauth2").Strategy;
+const userdb=require("./db/models/userSchema")
 
 const clientid="220885124105-n55avtdvtf0mbs13d84do9fnqpr429bm.apps.googleusercontent.com"
 const clientsecret="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"
@@ -16,9 +20,57 @@ app.use(cors({
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.status(200).json("Hello World");
+//setup session
+
+app.use(session({
+    secret:"123456hihihihihihi",
+    resave:false,
+    saveUninitialized:true
+}))
+
+//initialize passport ->setup passport 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+    new OAuth2Strategy({
+        clientID:clientid,
+        clientSecret:clientsecret,
+        callbackURL:"/auth/google/callback",
+        scope:["profile","email"]
+    },
+    //above code return asyn data
+    async(accessToken,refreshToken,profile,done)=>{
+        try {
+            let user = await userdb.findOne({googleId:profile.id});
+
+            if(!user){   //user not present in database
+                user = new userdb({
+                    googleId:profile.id,
+                    displayName:profile.displayName,
+                    email:profile.emails[0].value,
+                    image:profile.photos[0].value
+                });
+                await user.save();  //saved in database
+            }
+
+            return done(null,user)
+        } catch (error) {
+            return done(error,null)
+        }
+    }
+    )
+)
+
+//session gives a encrypted data , we store that data using serialize and deserialize
+passport.serializeUser((user,done)=>{
+    done(null,user);
+})
+
+passport.deserializeUser((user,done)=>{
+    done(null,user);
 });
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
